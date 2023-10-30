20cc3e077d451613ff67931bd7076744c3898fe3
Rüya Bozcan <93367377+ruyabozcan@users.noreply.github.com>
2023-10-30 19:42:56 +0300

    Add files via upload

---
+++ DryPortlyRoutine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  /*int i;
+  for(int num=1;num<100;num++){
+    double f=1;
+    for(i=1;i<num;i++)
+        f*=i;
+    printf("%d!=%.0lf\n",i,f);      
+  }*/
+   printf("%d",sizeof(int));
+
+    
+  
+  
+  return 0;
+}
